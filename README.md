d87651de9716bc0de3255cceba3671d7773ef57f
Daniel <danielzuzevich@gmail.com>
2016-10-16 21:31:04 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Installation
+
+1.) npm install
+2.) npm start
+3.) http://localhost:8080
---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+      </div>
+    );
+  }
+}
