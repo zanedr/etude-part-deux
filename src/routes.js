7d91cb146dc465dacb5d1f7d64d6015af8749a96
Daniel Zuzevich <danielzuzevich@gmail.com>
2016-11-22 14:51:37 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
+import FirstComponent from './components/FirstComponent/FirstComponent';
 
 export default (
   <Route path="/" component={App}>
-    {/*<IndexRoute component={SomeComponent} /> */}
+    <IndexRoute component={FirstComponent} /> 
   </Route>
 );
