4006ad156d54f2c0cb3167dd169dc3b39a7ed291
Zane Russell <zaneroosa@gmail.com>
2017-07-31 17:28:55 -0600

    original commit

---
+++ mock-data/songs.js
@@ -0,0 +1,47 @@
+export const mockSongs = [
+    {
+        title: "Stairway to Heaven",
+        artist: "Led Zeppelin",
+        date_added: "",
+        priority: 4,
+        timestamps: [],
+        tab: "stairway.json",
+        audio: "./stubData/music/Stairway.mp3"
+    },
+    {
+        title: "Blues Claire",
+        artist: "Django Reinhardt",
+        date_added: "",
+        priority: 3,
+        timestamps: [],
+        tab: "bluesclaire",
+        audio: "./stubData/music/BluesClaire.mp3"
+    },
+    {
+        title: "So What",
+        artist: "Miles Davis",
+        date_added: "",
+        priority: 2,
+        timestamps: [],
+        tab: "",
+        audio: "./stubData/music/BluesClaire.mp3"
+    },
+    {
+        title: "Everybody Wants To Rule The World",
+        artist: "Tears for Fears",
+        date_added: "",
+        priority: 1,
+        timestamps: [],
+        tab: "",
+        audio: "./stubData/music/Everybody.mp3"
+    },
+    {
+        title: "Wake Me Up Before You Go Go",
+        artist: "Wham!",
+        date_added: "",
+        priority: 5,
+        timestamps: [],
+        tab: "",
+        audio: "./stubData/music/jesuswhy.mp3"
+    },
+]
