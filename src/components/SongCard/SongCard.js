cebd3703e51ff87b414fd67c5b85c96d8681732a
Jennifer Peavler <jenn.peavler@gmail.com>
2017-07-31 20:35:34 -0600

    more ui

--- src/components/SongCard/SongCard.js
+++ src/components/SongCard/SongCard.js
@@ -3,8 +3,15 @@ import React from 'react';
 export const SongCard =  ({ song }) => {
   return (
     <section className='song'>
-      <h2>Title:  {song.title}</h2>
-      <p>Artist: {song.artist}</p>
+      <div className='title-artist'>
+        <h2>Title:  {song.title}</h2>
+        <p>Artist: {song.artist}</p>
+      </div>
+      <div className='tab-audio'>
+        <p>tab</p>
+        <p>audio</p>
+        <img src={require('./checkmark.png')} />
+      </div>
     </section>
   )
 }
