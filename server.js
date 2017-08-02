a338cf4d8e628923a3443ec29368099468823572
Devin Dixon <devinisafoal@gmail.com>
2017-08-01 18:18:31 -0600

    update happy/sad paths in server

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080
+const port = process.env.PORT || 8080
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 const bodyParser = require('body-parser');
+const path = require('path');
+const express = require('express');
+const app = express();
 
 // using webpack-dev-server and middleware in development environment
 if(process.env.NODE_ENV !== 'production') {
-  var webpackDevMiddleware = require('webpack-dev-middleware');
-  var webpackHotMiddleware = require('webpack-hot-middleware');
-  var webpack = require('webpack');
-  var config = require('./webpack.config');
-  var compiler = webpack(config);
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const webpack = require('webpack');
+  const config = require('./webpack.config');
+  const compiler = webpack(config);
 
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
@@ -18,12 +21,6 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
-
-
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Serve index.html in dist folder
@@ -33,10 +30,9 @@ app.get('/', function(request, response) {
 
 //**************GET REQUESTS***********************//
 app.get('/api/v1/songs', (req, res) => {
-
   database('songs').select()
   .then(songs => {
-    songs.length ? res.status(200).json(songs) : res.status(404).send('No songs were found');
+    return songs.length ? res.status(200).json(songs) : res.status(404).send('No songs were found');
   })
   .catch(error => res.status(500).send(error));
 });
@@ -49,27 +45,25 @@ app.post('/api/v1/songs', (req, res) => {
   const tab = req.body.tab || '';
   const priority = req.body.priority || 5;
   const timestamps = req.body.timestamps || '';
- 
+
   if(!title.length) {
     return res.status(422).send({
-        error: 'Please include a song title.',
+      error: 'Please include a song title.',
     });
   }
-  database('songs').insert({ title,
-    artist, timestamps, audio, tab, priority},
-    'id')
+
+  database('songs').insert({ title, artist, timestamps, audio, tab, priority}, 'id')
   .then((newArtist) => {
     return res.status(201).send({
       success: `Song ${title} added to database.`,
-    });
+    })
+    .catch(error => res.status(500).send(error));
   });
 });
 
 //**************PATCH REQUESTS***********************//
 
 app.patch('/api/v1/songs', (req, res) => {
-  console.log('PATCH', req.body)
-
   const title = req.body.title
   const artist = req.body.artist || '';
   const audio = req.body.audio || '';
@@ -78,40 +72,56 @@ app.patch('/api/v1/songs', (req, res) => {
   const timestamps = req.body.timestamps || '';
   const id = req.body.id;
 
-  database('songs').where('id', id)
-    .update('title', title)
-    .update('artist', artist)
-    .update('audio', audio)
-    .update('tab', tab)
-    .update('priority', priority)
-    .update('timestamps', timestamps)
-    .then(() => {
-      return res.status(201).send({
-        success: `Song entitled ${title} updated to reflect changes.`
+  database('songs').where('id', id).select()
+  .then(song => {
+    if (!song.length) {
+      return res.status(404).send({
+        error: 'Song does not exist in database.'
       })
-    })
+    } else {
+      database('songs').where('id', song[0].id).update({
+        'title': title,
+        'artist': artist,
+        'audio': audio,
+        'tab': tab,
+        'priority': priority,
+        'timestamps': timestamps
+      })
+      .then(() => {
+        return res.status(201).send({
+          success: `Song entitled ${title} updated to reflect changes.`
+        })
+      })
+      .catch(error => res.status(500).send(error));
+    }
+  });
 });
 
 //**************DELETE REQUESTS***********************//
 app.delete('/api/v1/songs', (req, res) => {
   const title = req.query.title;
 
-  database('songs').where('title', title).del()
-  .then(() => {
-    return res.status(204).send({
-      success: `Song entitled ${title} has been deleted from database`,
-    });
-  })
-    .catch(() => {
-      return res.status(500);
-    });
+  database('songs').where('title', title).select()
+  .then(song => {
+    if (!song.length) {
+      return res.status(404).send('Song does not exist in database.')
+    } else {
+      database('songs').where('title', song[0].title).del()
+      .then(() => {
+        return res.status(204).send({
+          success: `Song entitled ${title} has been deleted from database`,
+        });
+      })
+      .catch(error => res.status(500).send(error));
+    }
+  });    
 });
 
-app.listen(PORT, function(error) {
+app.listen(port, function(error) {
   if (error) {
     console.error(error);
   } else {
-    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", port, port);
   }
 });
 
