cd661d7ce491f2e63b7e88e935c1bae8ab04d433
Jennifer Peavler <jenn.peavler@gmail.com>
2017-08-01 18:16:55 -0600

    songs now listed in alphabetical order

--- src/components/SongDisplay/SongDisplay.js
+++ src/components/SongDisplay/SongDisplay.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { SongCard } from '../SongCard/SongCard'
 
 export const SongDisplay =  ({ songs, select }) => {
+  const songsInOrder = songs.sort((a,z) => {
+        return a.artist < z.artist ? -1 : 1
+      })
+  console.log(songsInOrder);
   return (
     <section id='songs-container'>
-      {songs.map((song, index) => {
+      {songsInOrder.map((song, index) => {
         return <SongCard song={song} key={index} select={select}/>
       })}
     </section>
