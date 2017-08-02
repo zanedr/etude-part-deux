49772e8f7c88e2c0c82cb775c36f31bf441ccf3f
Jennifer Peavler <jenn.peavler@gmail.com>
2017-08-01 23:13:27 -0600

    add Settings test file

---
+++ src/components/Settings/Settings.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import fetchMock from 'fetch-mock';
+
+import { Settings } from './Settings';
+
+describe('Settings', () => {
+  it('should render a settings container', () => {
+    console.log('i am a test')
+  });
+});
